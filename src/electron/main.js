b2e2ec2efadaa768c289e683f3aa4531e2c31330
Assassxn <adnanalepo2005@gmail.com>
2025-02-22 01:18:51 +0000

    next.js startup template for react/nextjs/electron

---
+++ src/electron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+require("dotenv").config();
+let mainWindow;
+
+app.on("ready", () => {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    const isDev = process.env.NODE_ENV === "development";
+    if (isDev) {
+        mainWindow.loadURL("http://localhost:3000"); // Load Next.js dev server
+    } else {
+        mainWindow.loadURL(`file://${path.join(__dirname, "../out/index.html")}`);
+    }
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
